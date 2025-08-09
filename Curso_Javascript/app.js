cf0190df058bbcfe229d98fcce6c3ff5c330b935
FrontCohete <jcoheter.fe@gmail.com>
2025-08-08 22:41:43 -0600

    Cambio

--- Curso_Javascript/app.js
+++ Curso_Javascript/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let maxRandom = 10;
 let numeroSecreto = Math.floor(Math.random()*maxRandom)+1;
 let entradaUser = 0;
@@ -42,48 +41,3 @@ function _mostrar_Numero(item, texto){
     itemHTML.innerHTML=texto;
 
 }
-=======
-let maxRandom = 10;
-let numeroSecreto = Math.floor(Math.random()*maxRandom)+1;
-let entradaUser = 0;
-let intentos = 1;
-let maxIntentos = 3;
-
-    console.log(numeroSecreto);
-
-while(intentos <= maxIntentos){    
-
-    entradaUser = prompt("Elija un número entre el 1 y 10: ");
-
-
-    if(numeroSecreto == entradaUser){
-        //alert("BIEN HECHO! ""   era el número a adivinar");
-        alert(`BIEN HECHO!\n ${entradaUser} era el número a adivinar, lo conseguiste en ${intentos} ${intentos==1 ? 'vez' : 'veces'}`);
-        //ALT + 96 para ``
-        break;
-    }
-    else{
-        if(entradaUser>numeroSecreto){
-            alert(`El número es mayor al que se debe adivinar, llevas ${intentos} intentos, te restan ${maxIntentos-intentos}`);
-        }
-        else{   
-            alert(`El número es menor al que se debe adivinar, llevas ${intentos} intentos, te restan ${maxIntentos-intentos}`);
-        } 
-        
-        if(intentos>maxIntentos){
-            alert("Llegaste al numero maximo de intentos");
-            break;
-            
-        }
-
-    }   
-
-    intentos = intentos +1;
-}
-
-function _mostrar_Numero(item, texto){
-    let itemHTML = document.querySelector(item);
-    itemHTML.innerHTML=texto;
-
-}
->>>>>>> 8ff5e53b1a2e72214a89c916da676d32b940f7af
--- Curso_Javascript_II/app.js
+++ Curso_Javascript_II/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const limiteRandom = 10;
 let n_intentos = 3;
 const num_random = _generarRandom();
@@ -48,55 +47,4 @@ function _main(){
 }
 
 
-=======
-const limiteRandom = 10;
-let n_intentos = 3;
-const num_random = _generarRandom();
-
-
-function _generarRandom() {
-    return Math.floor(Math.random()*limiteRandom)+1;
-}
-
-function _asignarTexto(elemento, texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML=texto;
-
-}
-
-function _send(){
-    let num_User = parseInt(document.getElementById('valor_User').value);
-    
-    n_intentos= n_intentos-1;
-
-    if(num_User===num_random){
-    _asignarTexto(`p`, `ADIVINASTE!`); 
-    }
-    else{
-        if(num_User < num_random){
-            _asignarTexto(`p`, `El numero secreto es menor || Tienes ${n_intentos} intentos :D`);
-        }
-        else{
-            _asignarTexto(`p`, `El numero secreto es menor || Tienes ${n_intentos} intentos :D`);
-        }
-    } 
-
-    if(n_intentos<0){
-        alert(`LLEGASTE AL LIMITE DE INTENTOS`);
-        _asignarTexto(`p`, `Te quedaste sin intentos, el numero era ${num_random}`);
-
-        document.getElementById('valor_User').disabled = true;
-    }
-
-
-}
-
-function _main(){
-    _asignarTexto(`h1`, `Adivina el número || No es ${num_random}`);    
-    _asignarTexto(`p`, `Tienes ${n_intentos} intentos :D`);
-    _asignarTexto(`label`, `Adivina el numéro del 1 al ${limiteRandom}`);   
-}
-
-
->>>>>>> 8ff5e53b1a2e72214a89c916da676d32b940f7af
 _main();
