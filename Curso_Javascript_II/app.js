7deeef073b1ca5271e95ff4fd98b0db438197667
FrontCohete <jcoheter.fe@gmail.com>
2025-08-10 21:23:26 -0600

    Juego terminado

--- Curso_Javascript_II/app.js
+++ Curso_Javascript_II/app.js
@@ -1,6 +1,6 @@
 const limiteRandom = 10;
 let n_intentos = 3;
-const num_random = _generarRandom();
+let num_random = _generarRandom();
 
 
 function _generarRandom() {
@@ -17,35 +17,32 @@ function _send(){
     let num_User = parseInt(document.getElementById('valor_User').value);
     
     n_intentos= n_intentos-1;
-
-    if(num_User===num_random){
-    _asignarTexto(`p`, `ADIVINASTE!`); 
-    _end();
-    }
-    else{
-        if(num_User < num_random){
-            _asignarTexto(`p`, `El numero secreto es menor || Tienes ${n_intentos} intentos :D`);
-        }
-        else{
-            _asignarTexto(`p`, `El numero secreto es mayor || Tienes ${n_intentos} intentos :D`);
-        }
-    } 
+ 
 
     if(n_intentos<0){
         alert(`LLEGASTE AL LIMITE DE INTENTOS`);
         _asignarTexto(`p`, `Te quedaste sin intentos, el numero era ${num_random}`);
         _end();
     }
+    else{
+        if(num_User===num_random){
+        _asignarTexto(`p`, `ADIVINASTE!`); 
+        _end();
+        }
+        else{
+            if(num_User < num_random){
+                _asignarTexto(`p`, `El numero secreto es menor || Tienes ${n_intentos} intentos :D`);
+            }
+            else{
+                _asignarTexto(`p`, `El numero secreto es mayor || Tienes ${n_intentos} intentos :D`);
+            }
+        }
+    }
 
     _limpiarTxtBox();
 
 }
 
-function _main(){
-    _asignarTexto(`h1`, `Adivina el número || No es ${num_random}`);    
-    _asignarTexto(`p`, `Tienes ${n_intentos} intentos :D`);
-    _asignarTexto(`label`, `Adivina el numéro del 1 al ${limiteRandom}`);   
-}
 
 function _limpiarTxtBox(){
     let valorBox = document.querySelector('#valor_User');
@@ -53,11 +50,33 @@ function _limpiarTxtBox(){
 }
 
 function _end(){
-        document.getElementById('valor_User').disabled = true;
-        document.getElementById('btn_Reset_App').removeAttribute('disabled') = false;
+    document.getElementById('valor_User').disabled = true;
+    document.querySelector('#btn_Send_App').setAttribute('disabled', 'true');
+    document.querySelector('#btn_Reset_App').removeAttribute('disabled')
 }
 
 function _reset(){
-    alert(`HOLACRAYOLA`);
+
+    alert(`Se generó un nuevo numero secreto`);
+    //Reestablecer valores
+    n_intentos = 3;
+    num_random = _generarRandom() 
+    // Habilitar btnSend & txtBx y Deshabilitar btnReset
+    document.querySelector('#btn_Send_App').removeAttribute('disabled')
+    document.getElementById('valor_User').disabled = false;
+    document.querySelector('#btn_Reset_App').setAttribute('disabled', 'true');
+    // Habilitar Mensajes iniciales
+    _main();
+
+
+    // |-> Funcion para recargar la página //location.reload(); 
 }
+
+function _main(){
+
+    _asignarTexto(`h1`, `Adivina el número || No es ${num_random}`);    
+    _asignarTexto(`p`, `Tienes ${n_intentos} intentos :D`);
+    _asignarTexto(`label`, `Adivina el numéro del 1 al ${limiteRandom}`);   
+}
+
 _main();
